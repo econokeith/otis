49eb5ad45abc6326bee674247044b9844c56ec76
econokeith <keith.blackwell@gmail.com>
2022-12-10 13:17:17 -0500

    working on otis_meets_keith

--- otis/test_scripts/learning_media_pipe.py
+++ otis/test_scripts/learning_media_pipe.py
@@ -9,17 +9,19 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
 
-drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1, color=(0, 255, 0))
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1, color=(0,255,0))
 style = mp_drawing.DrawingSpec(color=(0,255,0), thickness=1)
 cap = cv2.VideoCapture(0)
 tick = time.time()
 black_screen = np.zeros((720, 1080, 3), dtype='uint8')
-
+BLACK_SCREEN = False
 with mp_face_mesh.FaceMesh(
-        max_num_faces=1,
+
+        max_num_faces=3,
         refine_landmarks=True,
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as face_mesh:
+
     while cap.isOpened():
         success, image = cap.read()
         if not success:
@@ -32,11 +34,12 @@ with mp_face_mesh.FaceMesh(
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = face_mesh.process(image)
-
         # Draw the face mesh annotations on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        image[:,:,:]=0
+
+        if BLACK_SCREEN is True:
+            image[:,:,:]=0
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
                 mp_drawing.draw_landmarks(
@@ -74,9 +77,14 @@ with mp_face_mesh.FaceMesh(
         tick = tock
         # cv2.resize(image, (0,0), fx=2, fy=2)
 
-        cv2.imshow('MediaPipe Face Mesh', cv2.resize(image, (0,0), fx=2, fy=2))
-        if cv2.waitKey(5) & 0xFF == ord('q'):
+        cv2.imshow('MediaPipe Face Mesh', cv2.resize(image, (0,0), fx=2.5, fy=2.5))
+        key_input = cv2.waitKey(1) & 0xFF
+        if key_input == ord('1'):
+            BLACK_SCREEN = not BLACK_SCREEN
+        elif key_input == ord('q'):
             break
+        else:
+            pass
 
 cap.release()
 cv2.destroyAllWindows()
